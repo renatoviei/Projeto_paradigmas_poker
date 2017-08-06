fd8d4296f6cb01366ac53a277f65744368acfb58
Renato Vieira de Menezes <renatoviei@gmail.com>
2017-08-06 18:56:44 -0300

    Correção do método de ordenação.

--- src/beans/Mao.java
+++ src/beans/Mao.java
@@ -15,31 +15,28 @@ public class Mao {
 		if (mao.length == 5)
 			this.cartas = mao;
 		else
-			System.out.println("TAMANHO DA MAO DIFERENTE DE 5");
+			System.out.println("TAMANHO DA MAO MAIOR QUE 5");
 	}
 
 	// FUNÇAO QUE RETORNA A MAO ORDENADA DO MENOR VALOR ATE O MAIOR
-	public Mao ordena(Mao mao) //ok
+	public Mao ordena(Mao mao)
 
 	{
 		Mao maoAux = mao;
-		int atribuiPeso = 0;
-		while (atribuiPeso < mao.getCartas().length) {
-			maoAux.getCartas()[atribuiPeso].atribuirPeso(maoAux.getCartas()[atribuiPeso]);
-			atribuiPeso++;
-		}
+
 		int i, j, min;
 		Carta x;
 		for (i = 0; i < maoAux.getCartas().length - 1; ++i) {
 			min = i;
-			for (j = i + 1; j < mao.getCartas().length; ++j) {
-				if (maoAux.getCartas()[j].getPeso() < maoAux.getCartas()[min].getPeso()) {
+			for (j = i + 1; j < maoAux.getCartas().length; ++j) {
+				if (maoAux.getCartas()[j].getPeso() < maoAux.getCartas()[min].getPeso())
 					min = j;
-					x = maoAux.getCartas()[i];
-					maoAux.getCartas()[i] = maoAux.getCartas()[min];
-					maoAux.getCartas()[min] = x;
+			}
+			if (maoAux.getCartas()[i] != maoAux.getCartas()[min]) {
+				x = maoAux.getCartas()[i];
+				maoAux.getCartas()[i] = maoAux.getCartas()[min];
+				maoAux.getCartas()[min] = x;
 
-				}
 			}
 		}
 		return maoAux;
@@ -49,8 +46,7 @@ public class Mao {
 	// METODO QUE RETORNA A CARTA DE MAIOR VALOR DA MAO, O INT N É O NUMERO DE VEZES
 	// NECESSARIAS QUE O METODO FOI CHAMADO ATE DESEMPATAR O JOGO
 	public Carta cartaAlta(Mao mao, int n) {
-		//Mao ordenada = ordena(mao);        //NAO PRECISA ESTÁ AQUI... POIS QUANDO O METODO FOR CHAMADO, A MAO JA TEM Q ESTÁ ORDENADA (VAI SER DESPERDICIO DE PROCESSAMENTO)
-		Mao ordenada = mao;
+		Mao ordenada = ordena(mao);
 		return ordenada.getCartas()[4 - n];
 	}
 
@@ -80,8 +76,9 @@ public class Mao {
 		for (int i = 0; i < ordenada.getCartas().length - 1; i++) {
 			for (int j = i + 1; j < mao.getCartas().length; j++) {
 				if (ordenada.getCartas()[i].getNumero() == ordenada.getCartas()[j].getNumero()) {
-					
-					//ARMAZENA O VALOR DA CARTA PARA VERIFICAR SE ELE NÃO SE REPETE NO SEGUNDO TESTE
+
+					// ARMAZENA O VALOR DA CARTA PARA VERIFICAR SE ELE NÃO SE REPETE NO SEGUNDO
+					// TESTE
 					aux = ordenada.getCartas()[i].getNumero();
 					continue;
 
@@ -114,84 +111,54 @@ public class Mao {
 	}
 
 	// METODO QUE VERIFICA SE A MAO POSSUI UMA TRINCA
-	public boolean trinca(Mao mao) { //ok
+	public boolean trinca(Mao mao) {
 		boolean teste = false;
-		int cont1 = 0;
-		int cont2 = 0;
-		int cont3 = 0;
-		int cont4 = 0;
-		int cont5 = 0;
-		char primeiro = mao.getCartas()[0].getNumero();
-		char segundo = mao.getCartas()[1].getNumero();
-		char terceiro = mao.getCartas()[2].getNumero();
-		char quarto = mao.getCartas()[3].getNumero();
-		char quinto = mao.getCartas()[4].getNumero();;
-		//Mao ordenada = ordena(mao);
-		for (int i = 0; i < mao.getCartas().length; i++) {
+		Mao ordenada = ordena(mao);
+		for (int i = 0; i < ordenada.getCartas().length - 2; i++) {
 			// VERIFICA 3 POSIÇOES COM O MESMO VALOR
-			if ((i != 0) && primeiro == mao.getCartas()[i].getNumero())
-				cont1++;
-			if ((i != 1) && segundo == mao.getCartas()[i].getNumero())
-				cont2++;
-			if((i != 2) && terceiro == mao.getCartas()[i].getNumero())
-				cont3++;
-			if((i != 3) && quarto == mao.getCartas()[i].getNumero())
-				cont4++;
-			if ((i != 4) && quinto == mao.getCartas()[i].getNumero())
-				cont5++;
+			if (ordenada.getCartas()[i].getNumero() == ordenada.getCartas()[i + 1].getNumero()
+					&& ordenada.getCartas()[i + 1].getNumero() == ordenada.getCartas()[i + 2].getNumero()) {
+
+				teste = true;
+				break;
+			}
 		}
-		if (cont1 == 3 || cont2 == 3 || cont3 == 3 || cont4 == 3 || cont5 == 3)
-			teste= true;
 		return teste;
 	}
 
-	// METODO QUE VERIFICA SE A MAO POSSUI UM FULL HOUSE, UMA TRINCA E UM PAR, O VALOR NAO IMPORTA
-	public boolean fullHouse(Mao mao) { //ok
+	// METODO QUE VERIFICA SE A MAO POSSUI UM FULL HOUSE, UMA TRINCA E UM PAR, O
+	// VALOR NAO IMPORTA
+	public boolean fullHouse(Mao mao) {
 		boolean teste = false;
 		Mao ordenada = ordena(mao);
-		if ((umPar(ordenada) == true) && (trinca(ordenada) == true)) {
+		if (umPar(ordenada) == true && trinca(ordenada) == true) {
+
 			teste = true;
 		}
 		return teste;
 	}
 
 	// METODO QUE VERIFICA SE A MAO POSSUI UMA SEQUENCIA COM 5 VALORES CONSECUTIVOS
-	public boolean sequenciaStraight(Mao mao) { //ok
+	public boolean sequenciaStraight(Mao mao) {
 		boolean teste = false;
-		//Mao ordenada = ordena(mao);
-		int primeira = mao.getCartas()[0].getPeso();
-		int segunda = mao.getCartas()[1].getPeso();
-		int terceira = mao.getCartas()[2].getPeso();
-		int quarta = mao.getCartas()[3].getPeso();
-		int quinta = mao.getCartas()[4].getPeso();
-		
-		primeira++;
-		if(primeira == segunda){
-			primeira--;
-			segunda++;
-			if(segunda == terceira){
-				segunda--;
-				terceira++;
-				if(terceira == quarta){
-					terceira--;
-					quarta++;
-					if(quarta == quinta){
-						quarta--;
-						teste = true;							
-					}
-				}
+		Mao ordenada = ordena(mao);
+
+		for (int i = 0; i < ordenada.getCartas().length - 1; i++) {
+			if (ordenada.getCartas()[i].getPeso() < ordenada.getCartas()[i + 1].getPeso()) {
+
+				teste = true;
 			}
 		}
 		return teste;
 	}
 
 	// METODO QUE VERIFICA SE A MAO POSSUI AS CINCO CARTAS COM O MESMO NIPE
-	public boolean flush(Mao mao) { //OK
-		boolean teste = true;
-		//Mao ordenada = ordena(mao);
-		for (int j = 1; j < mao.getCartas().length; j++) {
-			if (mao.getCartas()[0].getNipe() != mao.getCartas()[j].getNipe()) {  //CASO ALGUMA CARTA TENHA NIPE DIFERENTE DAS DEMAIS, RETORNA FALSE. SE FOREM TODAS DO MESMO NIPE, RETORNA TRUE
-				teste = false;
+	public boolean flush(Mao mao) {
+		boolean teste = false;
+		Mao ordenada = ordena(mao);
+		for (int j = 1; j < ordenada.getCartas().length; j++) {
+			if (ordenada.getCartas()[0].getNipe() == ordenada.getCartas()[j].getNipe()) {
+				teste = true;
 			}
 		}
 
@@ -200,31 +167,26 @@ public class Mao {
 
 	// METODO QUE VERIFICA SE A MAO POSSUI AS CINCO CARTAS COM O MESMO NIPE E COM
 	// VALORES CONSECUTIVOS
-	public boolean straightFlush(Mao mao) { //ok
+	public boolean straightFlush(Mao mao) {
 		boolean teste = false;
-		//Mao ordenada = ordena(mao);
-		if ((this.flush(mao) == true) && (this.sequenciaStraight(mao) == true)) {
-			teste = true;	 
+		Mao ordenada = ordena(mao);
+		if (sequenciaStraight(ordenada) == true && flush(ordenada) == true) {
+			teste = true;
 		}
 		return teste;
 	}
 
 	// METODO QUE VERIFICA SE É UM ROYAL FLASH
-	public boolean royalFlash(Mao mao) { //OK
+	public boolean royalFlash(Mao mao) {
 		boolean teste = false;
-		//Mao ordenada = ordena(mao);
-		if (this.flush(mao) == true) { // FALTA A CONDIÇÃO PARA VER SE TEM 10, Valete, Dama, Rei e Ás
-			if (mao.getCartas()[0].getNumero() == 'T' && mao.getCartas()[1].getNumero() == 'J'
-					&& mao.getCartas()[2].getNumero() == 'Q' && mao.getCartas()[3].getNumero() == 'K'
-					&& mao.getCartas()[4].getNumero() == 'A') {
+		Mao ordenada = ordena(mao);
+		if (flush(mao) == true) { // FALTA A CONDIÇÃO PARA VER SE TEM 10, Valete, Dama, Rei e Ás
+			if (ordenada.getCartas()[0].getNumero() == 'T' && ordenada.getCartas()[1].getNumero() == 'J'
+					&& ordenada.getCartas()[2].getNumero() == 'Q' && ordenada.getCartas()[3].getNumero() == 'K'
+					&& ordenada.getCartas()[4].getNumero() == 'A') {
 				teste = true;
 			}
 		}
 		return teste;
-	}
-	
-	public static void main (String[] args){
-		//Carta a = new Carta ();
-		
 	}
 }
--- src/beans/Principal.java
+++ src/beans/Principal.java
@@ -3,7 +3,6 @@ package beans;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
 
 public class Principal {
 
@@ -11,18 +10,17 @@ public class Principal {
 		// TODO Auto-generated method stub
 		FileReader file = new FileReader("artefatos\\pokerK.txt");
 		BufferedReader arq = new BufferedReader(file);
-		
+
 		String linha = "";
 		String maos[];
-		
+
 		char numero = '0';
 		char nipe = '0';
-		int ind1 = 0;
 		int ind = 0;
-		
+
 		int vitorias1 = 0;
 		int vitorias2 = 0;
-				
+
 		Carta carta = null;
 		Carta cartas1[] = new Carta[5];
 		Carta cartas2[] = new Carta[5];
@@ -31,9 +29,8 @@ public class Principal {
 		Jogador j1 = null;
 		Jogador j2 = null;
 		Poker jogo = null;
-		
-		
-		while  ((linha = arq.readLine()) != null) {
+
+		while ((linha = arq.readLine()) != null) {
 			maos = linha.split(" ");
 			for (int i = 0; i < 10; i++) {
 				numero = maos[i].charAt(0);
@@ -49,53 +46,51 @@ public class Principal {
 				if (i > 4) {
 					cartas2[ind] = carta;
 				}
-				ind++;				
+				ind++;
 			}
 			ind = 0;
+
 			mao1 = new Mao(cartas1);
 			mao1.ordena(mao1);
 			mao2 = new Mao(cartas2);
 			mao2.ordena(mao2);
-			
-			j1 = new Jogador (mao1, vitorias1);
-			j2 = new Jogador (mao2, vitorias2);
-			
+
+			j1 = new Jogador(mao1, vitorias1);
+			j2 = new Jogador(mao2, vitorias2);
+
 			jogo = new Poker(j1, j2);
-			
+
 			char numero1 = '-';
 			char nipe1 = '+';
 			char numero2 = '-';
 			char nipe2 = '+';
-			
+
 			System.out.print("jogador 1: ");
 			for (int i = 0; i < 5; i++) {
-				numero1 = jogo.getJogador1().getMao().getCartas()[i].getNumero(); 
+				numero1 = jogo.getJogador1().getMao().getCartas()[i].getNumero();
 				nipe1 = jogo.getJogador1().getMao().getCartas()[i].getNipe();
 				System.out.print(numero1);
 				System.out.print(nipe1);
 				System.out.print(" ");
 			}
 			System.out.println();
-			
+
 			System.out.print("jogador 2: ");
 			for (int i = 0; i < 5; i++) {
 				numero2 = jogo.getJogador2().getMao().getCartas()[i].getNumero();
-				nipe2 = jogo.getJogador1().getMao().getCartas()[i].getNipe();
+				nipe2 = jogo.getJogador2().getMao().getCartas()[i].getNipe();
 				System.out.print(numero2);
 				System.out.print(nipe2);
 				System.out.print(" ");
 			}
 			System.out.println();
 			System.out.println();
-			
+
 		}
-		
+
 		arq.close();
 		file.close();
-		
-		
-		}
 
 	}
 
-
+}
